37077c211a720fe7d7f0f71413395d0132ab84cc
OhNishant <nishant.mishra550@gmail.com>
2023-12-18 11:27:21 +0530

    log errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("[Error] Problem parsing arguments: {}", err);
+        eprintln!("[Error] Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = lilgrep::run(config) {
-        println!("[Error]: {e}");
+        eprintln!("[Error]: {e}");
         process::exit(1);
     }
 }
