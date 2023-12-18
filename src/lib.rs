a7f55d2d34b407149ad379c633b69be26addd178
OhNishant <nishant.mishra550@gmail.com>
2023-12-18 09:45:27 +0530

    yuh

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    return Ok(());
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough args");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        return Ok(Config { query, file_path });
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = vec![];
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    return results;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use lilgrep::Config;
+
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("[Error] Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = lilgrep::run(config) {
+        println!("[Error]: {e}");
+        process::exit(1);
+    }
 }
